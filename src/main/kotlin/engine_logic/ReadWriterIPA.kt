d7c99baf0b5408a879d2a6ef7bfc88b248e3825e
hispanicdevian <iantastream@gmail.com>
2024-01-06 10:40:45 -0800

    Updating to match private version changes 2

--- src/main/kotlin/engine_logic/ReadWriterIPA.kt
+++ src/main/kotlin/engine_logic/ReadWriterIPA.kt
@@ -2,10 +2,10 @@ package engine_logic
 
 import java.io.File
 
-// 00
+//////////////////////////////////////////////////////////// Read/Write File for Boxes IPs value 00
 fun readIpFile0(): String {
     return try {
-        val file0 = File("file0.txt").readText()
+        val file0 = File("rwIP00.hdi").readText()
         file0
     } catch (e: Exception) {
         ""
@@ -13,18 +13,19 @@ fun readIpFile0(): String {
 }
 fun writeIpToFile0(ipAddress: String) {
     try {
-        val file0 = File("file0.txt")
+        val file0 = File("rwIP00.hdi")
         file0.writeText(ipAddress)
         ipAddress0 = ipAddress // Update the in-memory value
     } catch (e: Exception) {
-        // Handle exceptions
+        // Log the exception
+        e.printStackTrace()
     }
 }
 
-// 01
+//////////////////////////////////////////////////////////// Read/Write File for Boxes IPs value 01
 fun readIpFile1(): String {
     return try {
-        val file1 = File("file1.txt").readText()
+        val file1 = File("rwIP01.hdi").readText()
         file1
     } catch (e: Exception) {
         ""
@@ -32,18 +33,19 @@ fun readIpFile1(): String {
 }
 fun writeIpToFile1(ipAddress: String) {
     try {
-        val file1 = File("file1.txt")
+        val file1 = File("rwIP01.hdi")
         file1.writeText(ipAddress)
         ipAddress1 = ipAddress // Update the in-memory value
     } catch (e: Exception) {
-        // Handle exceptions
+        // Log the exception
+        e.printStackTrace()
     }
 }
 
-// 02
+//////////////////////////////////////////////////////////// Read/Write File for IPs value, Boxes 02
 fun readIpFile2(): String {
     return try {
-        val file2 = File("file2.txt").readText()
+        val file2 = File("rwIP02.hdi").readText()
         file2
     } catch (e: Exception) {
         ""
@@ -51,18 +53,19 @@ fun readIpFile2(): String {
 }
 fun writeIpToFile2(ipAddress: String) {
     try {
-        val file2 = File("file2.txt")
+        val file2 = File("rwIP02.hdi")
         file2.writeText(ipAddress)
         ipAddress2 = ipAddress // Update the in-memory value
     } catch (e: Exception) {
-        // Handle exceptions
+        // Log the exception
+        e.printStackTrace()
     }
 }
 
-//03
+//////////////////////////////////////////////////////////// Read/Write File for IPs value, Boxes 03
 fun readIpFile3(): String {
     return try {
-        val file3 = File("file3.txt").readText()
+        val file3 = File("rwIP03.hdi").readText()
         file3
     } catch (e: Exception) {
         ""
@@ -70,10 +73,11 @@ fun readIpFile3(): String {
 }
 fun writeIpToFile3(ipAddress: String) {
     try {
-        val file3 = File("file3.txt")
+        val file3 = File("rwIP03.hdi")
         file3.writeText(ipAddress)
         ipAddress3 = ipAddress // Update the in-memory value
     } catch (e: Exception) {
-        // Handle exceptions
+        // Log the exception
+        e.printStackTrace()
     }
 }
