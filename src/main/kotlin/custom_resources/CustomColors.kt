c54f6de89018ae45c27480d6e1b421f07da50598
hispanicdevian <iantastream@gmail.com>
2024-01-07 10:31:18 -0800

    Updating to match private version changes 31

--- src/main/kotlin/custom_resources/CustomColors.kt
+++ src/main/kotlin/custom_resources/CustomColors.kt
@@ -2,6 +2,7 @@ package custom_resources
 
 import androidx.compose.ui.graphics.Color
 
+//////////////////////////////////////////////////////////// Android Default Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -10,11 +11,13 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-//////////////////////////////////////////////////////////// NBT Colors 1
-val NbtColor = Color(0xFF008D97)
-val NbtRed = Color(4292294221)
+//////////////////////////////////////////////////////////// Main Color
+val MainColorA = Color(0xFF008D97)
 
-//////////////////////////////////////////////////////////// My Custom Colors
-val ErgoGray = Color(0xFF303030)
-val ErgoRed = Color(0xFFD7213A)
-val ErgoGreen = Color(0xFF0D5E36)
+//////////////////////////////////////////////////////////// Custom Colors A
+val CustomGrayA = Color(0xFF303030)
+val CustomGreenA = Color(0xFF0D5E36)
+val CustomRedA = Color(0xFFD7213A)
+
+//////////////////////////////////////////////////////////// Custom Colors B
+val CustomRedB = Color(4292294221)
