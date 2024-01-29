85e788844d1d8e5dd514da0b78338e7eb6bee212
hispanicdevian <iantastream@gmail.com>
2024-01-28 23:48:28 -0800

    Testing TextField to add placeholders

--- src/main/kotlin/custom_resources/CustomSpacers.kt
+++ src/main/kotlin/custom_resources/CustomSpacers.kt
@@ -7,19 +7,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////// Width
+
+/////////////// MainScreen.kt
 @Composable
 fun msSpacerH() { Spacer(modifier = Modifier.width(15.dp)) }
 
+/////////////// SettingPingBoxes.kt
 @Composable
 fun spbSpacerH() { Spacer(modifier = Modifier.width(10.dp)) }
 
+/////////////// SettingOnOffBoxes.kt
 @Composable
 fun soobSpacerH() { Spacer(modifier = Modifier.width(12.dp)) }
 
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////// Height
+
+/////////////// SettingFontSize.kt
 @Composable
 fun psbSpacerH() { Spacer(modifier = Modifier.height(10.dp)) }
-
-
 
