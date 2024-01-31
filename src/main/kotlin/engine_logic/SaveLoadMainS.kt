bc36f3dd19bfadc25e04fe6c5ac958b990c7eef8
hispanicdevian <iantastream@gmail.com>
2024-01-30 18:47:20 -0800

    matching both private and core version

--- src/main/kotlin/engine_logic/SaveLoadMainS.kt
+++ src/main/kotlin/engine_logic/SaveLoadMainS.kt
@@ -4,7 +4,12 @@ import java.io.File
 
 //////////////////////////////////////////////////////////// Save and Load A
 object SLOnOffHandlerA {
-    private const val FILEPATH0 = "msStateA.hdi"
+    private const val FILE_NAME0 = "msStateA.hdi"
+    private const val FILEPATH0 = "$SLM_PATH/$FILE_NAME0"
+
+    init {
+        File(SLM_PATH).mkdirs()
+    }
 
     fun loadVisibilityListA(): List<Boolean> {
         val currentState = loadOnOffFileA()
@@ -15,17 +20,17 @@ object SLOnOffHandlerA {
         }
     }
 
-    fun saveVisibilityListA(visibilityList: List<Boolean>) {
-        val stateAsString = visibilityList.joinToString(",")
-        saveOnOffFileA(stateAsString)
-    }
-
     fun loadOnOffFileA(): String {
         return try {
             File(FILEPATH0).readText()
         } catch (e: Exception) {
             ""
         }
+    }
+
+    fun saveVisibilityListA(visibilityList: List<Boolean>) {
+        val stateAsString = visibilityList.joinToString(",")
+        saveOnOffFileA(stateAsString)
     }
 
     private fun saveOnOffFileA(scState: String) {
@@ -38,23 +43,22 @@ object SLOnOffHandlerA {
         }
     }
 }
-
 //////////////////////////////////////////////////////////// Save and Load B
 object SLOnOffHandlerB {
-    private const val FILEPATH1 = "msStateB.hdi"
+    private const val FILE_NAME1 = "msStateB.hdi"
+    private const val FILEPATH1 = "$SLM_PATH/$FILE_NAME1"
+
+    init {
+        File(SLM_PATH).mkdirs()
+    }
 
     fun loadVisibilityListB(): List<Boolean> {
-        val currentState = loadOnOffFileB()
+        val currentState = SLOnOffHandlerA.loadOnOffFileA()
         return if (currentState.isNotEmpty()) {
             currentState.split(",").map { it.trim().toBoolean() }
         } else {
             listOf(true, true, true, true)
         }
-    }
-
-    fun saveVisibilityListB(visibilityList: List<Boolean>) {
-        val stateAsString = visibilityList.joinToString(",")
-        saveOnOffFileB(stateAsString)
     }
 
     fun loadOnOffFileB(): String {
@@ -63,6 +67,11 @@ object SLOnOffHandlerB {
         } catch (e: Exception) {
             ""
         }
+    }
+
+    fun saveVisibilityListB(visibilityList: List<Boolean>) {
+        val stateAsString = visibilityList.joinToString(",")
+        saveOnOffFileB(stateAsString)
     }
 
     private fun saveOnOffFileB(scState: String) {
@@ -75,10 +84,14 @@ object SLOnOffHandlerB {
         }
     }
 }
-
 //////////////////////////////////////////////////////////// Save and Load C
 object SLOnOffHandlerC {
-    private const val FILEPATH2 = "msStateC.hdi"
+    private const val FILE_NAME2 = "msStateC.hdi"
+    private const val FILEPATH2 = "$SLM_PATH/$FILE_NAME2"
+
+    init {
+        File(SLM_PATH).mkdirs()
+    }
 
     fun loadVisibilityListC(): List<Boolean> {
         val currentState = loadOnOffFileC()
@@ -89,11 +102,6 @@ object SLOnOffHandlerC {
         }
     }
 
-    fun saveVisibilityListC(visibilityList: List<Boolean>) {
-        val stateAsString = visibilityList.joinToString(",")
-        saveOnOffFileC(stateAsString)
-    }
-
     fun loadOnOffFileC(): String {
         return try {
             File(FILEPATH2).readText()
@@ -102,47 +110,15 @@ object SLOnOffHandlerC {
         }
     }
 
+    fun saveVisibilityListC(visibilityList: List<Boolean>) {
+        val stateAsString = visibilityList.joinToString(",")
+        saveOnOffFileC(stateAsString)
+    }
+
     private fun saveOnOffFileC(scState: String) {
         try {
             val onOffFileC = File(FILEPATH2)
             onOffFileC.writeText(scState)
-        } catch (e: Exception) {
-            // Log the exception
-            e.printStackTrace()
-        }
-    }
-}
-
-//////////////////////////////////////////////////////////// Save and Load D
-object SLOnOffHandlerD {
-    private const val FILEPATH3 = "msStateD.hdi"
-
-    fun loadVisibilityListD(): List<Boolean> {
-        val currentState = loadOnOffFileD()
-        return if (currentState.isNotEmpty()) {
-            currentState.split(",").map { it.trim().toBoolean() }
-        } else {
-            listOf(true, true, true, true)
-        }
-    }
-
-    fun saveVisibilityListD(visibilityList: List<Boolean>) {
-        val stateAsString = visibilityList.joinToString(",")
-        saveOnOffFileD(stateAsString)
-    }
-
-    fun loadOnOffFileD(): String {
-        return try {
-            File(FILEPATH3).readText()
-        } catch (e: Exception) {
-            ""
-        }
-    }
-
-    private fun saveOnOffFileD(scState: String) {
-        try {
-            val onOffFileD = File(FILEPATH3)
-            onOffFileD.writeText(scState)
         } catch (e: Exception) {
             // Log the exception
             e.printStackTrace()
