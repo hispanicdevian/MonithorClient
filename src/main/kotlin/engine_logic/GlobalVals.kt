6064cd110d7df4515c9726ab6f63d73a663d579b
Ian Gastelum <135581442+hispanicdevian@users.noreply.github.com>
2024-01-31 00:28:09 -0800

    Update GlobalVals.kt

--- src/main/kotlin/engine_logic/GlobalVals.kt
+++ src/main/kotlin/engine_logic/GlobalVals.kt
@@ -14,19 +14,19 @@ var ipAddress9 = readIpFile9()
 var ipAddress10 = readIpFile10()
 var ipAddress11 = readIpFile11()
 
-////////////////////////////// PingSettingBoxesA, ReWrTitleA
+////////////////////////////// PingSettingBoxesA.kt, ReWrTitleA.kt
 var ipTitle00 = readTiFile0()
 var ipTitle01 = readTiFile1()
 var ipTitle02 = readTiFile2()
 var ipTitle03 = readTiFile3()
 
-////////////////////////////// PingSettingBoxesB, ReWrTitleB
+////////////////////////////// PingSettingBoxesB.kt, ReWrTitleB.kt
 var ipTitle04 = readTiFile4()
 var ipTitle05 = readTiFile5()
 var ipTitle06 = readTiFile6()
 var ipTitle07 = readTiFile7()
 
-////////////////////////////// PingSettingBoxesC, ReWrTitleC
+////////////////////////////// PingSettingBoxesC.kt, ReWrTitleC
 var ipTitle08 = readTiFile8()
 var ipTitle09 = readTiFile9()
 var ipTitle10 = readTiFile10()
