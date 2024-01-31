f10e8d2f138c6f540c9ff75ca3ef84c4574c439d
hispanicdevian <iantastream@gmail.com>
2024-01-30 18:44:49 -0800

    matching both private and core version

--- src/main/kotlin/engine_logic/ReWrTitleA.kt
+++ src/main/kotlin/engine_logic/ReWrTitleA.kt
@@ -3,77 +3,78 @@ package engine_logic
 import java.io.File
 
 //////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 00
-fun readTiFile0(): String {
-    return try {
-        val file0 = File("rwTi00.hdi").readText()
-        file0
-    } catch (e: Exception) {
-        ""
-    }
-}
 fun writeTiToFile0(ipTitle: String) {
     try {
-        val file0 = File("rwTi00.hdi")
+        val file0 = File("$RWT_PATH/rwTi00.hdi")
+        file0.parentFile?.mkdirs()
         file0.writeText(ipTitle)
         ipTitle00 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
     }
 }
-
-//////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 01
-fun readTiFile1(): String {
+fun readTiFile0(): String {
     return try {
-        val file1 = File("rwTi01.hdi").readText()
-        file1
+        val file0 = File(RWT_PATH,"rwTi00.hdi").readText()
+        file0
     } catch (e: Exception) {
         ""
     }
 }
+//////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 01
 fun writeTiToFile1(ipTitle: String) {
     try {
-        val file1 = File("rwTi01.hdi")
+        val file1 = File("$RWT_PATH/rwTi01.hdi")
+        file1.parentFile?.mkdirs()
         file1.writeText(ipTitle)
         ipTitle01 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
     }
 }
-
-//////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 02
-fun readTiFile2(): String {
+fun readTiFile1(): String {
     return try {
-        val file2 = File("rwTi02.hdi").readText()
-        file2
+        val file1 = File(RWT_PATH,"rwTi01.hdi").readText()
+        file1
     } catch (e: Exception) {
         ""
     }
 }
+//////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 02
 fun writeTiToFile2(ipTitle: String) {
     try {
-        val file2 = File("rwTi02.hdi")
+        val file2 = File("$RWT_PATH/rwTi02.hdi")
+        file2.parentFile?.mkdirs()
         file2.writeText(ipTitle)
         ipTitle02 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
     }
 }
-
-//////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 03
-fun readTiFile3(): String {
+fun readTiFile2(): String {
     return try {
-        val file3 = File("rwTi03.hdi").readText()
-        file3
+        val file2 = File(RWT_PATH,"rwTi02.hdi").readText()
+        file2
     } catch (e: Exception) {
         ""
     }
 }
+//////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 03
 fun writeTiToFile3(ipTitle: String) {
     try {
-        val file3 = File("rwTi03.hdi")
+        val file3 = File("$RWT_PATH/rwTi03.hdi")
+        file3.parentFile?.mkdirs()
         file3.writeText(ipTitle)
         ipTitle03 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
+    }
+}
+fun readTiFile3(): String {
+    return try {
+        val file3 = File(RWT_PATH,"rwTi03.hdi").readText()
+        file3
+    } catch (e: Exception) {
+        ""
     }
 }
--- src/main/kotlin/engine_logic/ReWrTitleB.kt
+++ src/main/kotlin/engine_logic/ReWrTitleB.kt
@@ -3,77 +3,78 @@ package engine_logic
 import java.io.File
 
 //////////////////////////////////////////////////////////// Read/Write File for Boxes Title value 04
-fun readTiFile4(): String {
-    return try {
-        val file4 = File("rwTi04.hdi").readText()
-        file4
-    } catch (e: Exception) {
-        ""
-    }
-}
 fun writeTiToFile4(ipTitle: String) {
     try {
-        val file4 = File("rwTi04.hdi")
+        val file4 = File("$RWT_PATH/rwTi04.hdi")
+        file4.parentFile?.mkdirs()
         file4.writeText(ipTitle)
         ipTitle04 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
     }
 }
-
-//////////////////////////////////////////////////////////// Read/Write File for Boxes IPs value 05
-fun readTiFile5(): String {
+fun readTiFile4(): String {
     return try {
-        val file5 = File("rwTi05.hdi").readText()
-        file5
+        val file4 = File("$RWT_PATH/rwTi04.hdi").readText()
+        file4
     } catch (e: Exception) {
         ""
     }
 }
+//////////////////////////////////////////////////////////// Read/Write File for Boxes IPs value 05
 fun writeTiToFile5(ipTitle: String) {
     try {
-        val file5 = File("rwTi05.hdi")
+        val file5 = File("$RWT_PATH/rwTi05.hdi")
+        file5.parentFile?.mkdirs()
         file5.writeText(ipTitle)
         ipTitle05 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
     }
 }
-
-//////////////////////////////////////////////////////////// Read/Write File for IPs value, Boxes 06
-fun readTiFile6(): String {
+fun readTiFile5(): String {
     return try {
-        val file6 = File("rwTi06.hdi").readText()
-        file6
+        val file5 = File("$RWT_PATH/rwTi05.hdi").readText()
+        file5
     } catch (e: Exception) {
         ""
     }
 }
+//////////////////////////////////////////////////////////// Read/Write File for IPs value, Boxes 06
 fun writeTiToFile6(ipTitle: String) {
     try {
-        val file6 = File("rwTi06.hdi")
+        val file6 = File("$RWT_PATH/rwTi06.hdi")
+        file6.parentFile?.mkdirs()
         file6.writeText(ipTitle)
         ipTitle06 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
     }
 }
-
-//////////////////////////////////////////////////////////// Read/Write File for IPs value, Boxes 07
-fun readTiFile7(): String {
+fun readTiFile6(): String {
     return try {
-        val file7 = File("rwTi07.hdi").readText()
-        file7
+        val file6 = File("$RWT_PATH/rwTi06.hdi").readText()
+        file6
     } catch (e: Exception) {
         ""
     }
 }
+//////////////////////////////////////////////////////////// Read/Write File for IPs value, Boxes 07
 fun writeTiToFile7(ipTitle: String) {
     try {
-        val file7 = File("rwTi07.hdi")
+        val file7 = File("$RWT_PATH/rwTi07.hdi")
+        file7.parentFile?.mkdirs()
         file7.writeText(ipTitle)
         ipTitle07 = ipTitle // Updates the in-memory value
     } catch (e: Exception) { // Log the exception
         e.printStackTrace()
+    }
+}
+fun readTiFile7(): String {
+    return try {
+        val file7 = File("$RWT_PATH/rwTi07.hdi").readText()
+        file7
+    } catch (e: Exception) {
+        ""
     }
 }
