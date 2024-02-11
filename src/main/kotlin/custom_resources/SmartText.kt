e01828ec12f052e6fe16f58ed3f89883f23a8294
olracnai <bongohmo@gmail.com>
2024-02-11 10:32:39 -0800

    Adding smart text

---
+++ src/main/kotlin/custom_resources/SmartText.kt
@@ -0,0 +1,20 @@
+package custom_resources
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.WindowState
+
+@Composable
+// This right here is what you need; make sure to pass the value from /Main.kt/windowsState/initialWindowState
+fun smartText(windowState: WindowState): TextUnit {
+    val displayState = windowState.size.height
+
+    return when {
+        displayState <= 720.dp -> 25.sp
+        displayState in 730.dp..820.dp -> 30.sp
+        displayState >= 830.dp -> 40.sp
+        else -> 20.sp // Default size
+    }
+}
