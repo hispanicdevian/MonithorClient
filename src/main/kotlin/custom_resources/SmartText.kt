bb4a8d6e88dd896606fda65ffa35c3ce3ed0e130
olracnai <bongohmo@gmail.com>
2024-02-12 10:32:11 -0800

    smartText

--- src/main/kotlin/custom_resources/SmartText.kt
+++ src/main/kotlin/custom_resources/SmartText.kt
@@ -9,9 +9,10 @@ fun smartText(windowState: WindowState): TextUnit {
     val displayState = windowState.size.height
 
     return when {
-        displayState <= 720.dp -> 25.sp
+        displayState <= 720.dp -> 20.sp
         displayState in 730.dp..820.dp -> 30.sp
-        displayState >= 830.dp -> 40.sp
+        displayState in 830.dp..1100.dp -> 40.sp
+        displayState >= 1110.dp -> 50.sp
         else -> 20.sp // Default size
     }
 }
@@ -21,10 +22,11 @@ fun smartText(windowState: WindowState): TextUnit {
     val displayState = windowState.size.height
 
     val textSize = when {
-        displayState <=  720.dp ->  25.sp
-        displayState in  730.dp..820.dp ->  30.sp
-        displayState >=  830.dp ->  40.sp
-        else ->  20.sp // Default size
+        displayState <= 720.dp -> 20.sp
+        displayState in 730.dp..820.dp -> 30.sp
+        displayState in 830.dp..1100.dp -> 40.sp
+        displayState >= 1110.dp -> 50.sp
+        else -> 20.sp // Default size
     }
     return textSize
 }
